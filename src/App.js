f6eb85e51c7ddc0eac5bbdf9780b4fdcc58896a9
gabimancini <gabimancinir@gmail.com>
2023-10-26 12:19:28 -0400

    Add bg home

--- src/App.js
+++ src/App.js
@@ -3,8 +3,10 @@ import Footer from "./components/footer";
 import Router from "./routes/routes";
  
 function App() {
+  console.log(window.location.pathname)
   return (
      <>
+     {window.location.pathname === "/" && <div className="bg_home"></div> }
       <Header />
       <main>
         <Router />
