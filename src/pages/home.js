b9898dd415c49ab9f4a31d5574acbb41d9de3fa2
gabimancini <gabimancinir@gmail.com>
2023-10-27 10:58:37 -0400

    Add btn class

--- src/pages/home.js
+++ src/pages/home.js
@@ -5,7 +5,7 @@ export default function Home() {
             <div className="container container_center">
             <div style={{margin:"100px", display:"flex", flexDirection:'column', justifyContent:'center', alignItems:'center'}}>
                 <h1><span>Purple</span><span className="cLetter">C</span><span>ircle</span></h1>
-                <Link to='https://open.spotify.com/intl-es/artist/2Yjvtj6PygasEQoDJbtm1N?si=rvc82iviSBePvb7ZB01-Tw' rel="noreferrer" target='_blank' className="linkBtn">Listen Us</Link>
+                <Link to='https://open.spotify.com/intl-es/artist/2Yjvtj6PygasEQoDJbtm1N?si=rvc82iviSBePvb7ZB01-Tw' rel="noreferrer" target='_blank' className="linkBtn btnHome">Listen Us</Link>
                 </div>
             </div>
         </section>
